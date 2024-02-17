2f231e106518fe4254dd999780984e31a010c6ac
Chapin Bryce <mail@chapinb.com>
2024-02-17 08:54:21 -0500

    F!! Add new function to get the location of an IP, and updates for our testing tools to support the use of requests

--- app/app/ip_utils.py
+++ app/app/ip_utils.py
@@ -1,5 +1,7 @@
 import ipaddress
 
+import requests
+
 
 def is_ip_address(value: str) -> bool:
     try:
@@ -7,3 +9,20 @@ def is_ip_address(value: str) -> bool:
         return True
     except ValueError:
         return False
+
+
+def get_ip_location(value: str) -> dict[str, str | float]:
+    if not is_ip_address(value):
+        raise ValueError("The value is not an IP!")
+
+    response = requests.get(f"http://ip-api.com/json/{value}")
+    response.raise_for_status()
+
+    response_data = response.json()
+
+    return {
+        "lat": response_data.get("lat"),
+        "lon": response_data.get("lon"),
+        "city": response_data.get("city"),
+        "country": response_data.get("country"),
+    }
--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from app.ip_utils import is_ip_address
+from app.ip_utils import is_ip_address, get_ip_location
 
 
 class TestApp(TestCase):
@@ -11,3 +11,17 @@ class TestApp(TestCase):
         is_ip = is_ip_address(sample_data)
         # Assert
         self.assertTrue(is_ip)
+
+    def test_get_ip_location(self):
+        # Assemble
+        sample_data = "2.2.2.2"
+        # Action
+        actual_location = get_ip_location(sample_data)
+        # Assertion
+        expected_location = {
+            "lat": 48.8566,
+            "lon": 2.35222,
+            "city": "Paris",
+            "country": "France",
+        }
+        self.assertEqual(expected_location, actual_location)
